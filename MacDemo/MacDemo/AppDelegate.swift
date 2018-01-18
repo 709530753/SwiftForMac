365fb4ab905b188c93a345ab1e01ad5aa9a74ebb
YangXingChen <yangxingchen@senlime.com>
2018-01-18 12:04:31 +0800

    add baseUI

---
+++ MacDemo/MacDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacDemo
+//
+//  Created by fengxin on 18/01/2018.
+//  Copyright © 2018 fengxin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ MacDemo/MacDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MacDemo
+//
+//  Created by fengxin on 18/01/2018.
+//  Copyright © 2018 fengxin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+
+  @IBAction func didAddAccessPoint(_ sender: Any) {
+ 
+  print("add")
+  }
+  
+}
+
